cb96032db0f692412a8575010df1744e4a9b3ef7
Floch <gaelle_floch@hotmail.com>
2020-03-03 16:51:09 +0100

    erreur javascipt reorganisation carousel et map

--- wp-content/themes/startheme2020/src/js/main.js
+++ wp-content/themes/startheme2020/src/js/main.js
@@ -4,38 +4,6 @@ import 'leaflet';
 import 'bootstrap';
 
 $(document).ready(function () {
-
-    const themeURL = WPURLS.themeURL;
-
-    /* Spot Map */
-    const mapDiv = $('#spotMap');
-    const latitude = mapDiv.data('latitude');
-    const longitude = mapDiv.data('longitude');
-    const title = mapDiv.data('title');
-    const bsIcon = L.icon({
-        iconUrl: themeURL + '/dist/images/marker.png',
-        shadowUrl: themeURL + '/dist/images/marker-shadow.png',
-
-        iconSize: [38, 95], // size of the icon
-        shadowSize: [50, 60], // size of the shadow
-        iconAnchor: [22, 90], // point of the icon which will correspond to marker's location
-        shadowAnchor: [4, 62], // the same for the shadow
-        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-    });
-
-
-    /* Spot Map */
-    var map = L.map('spotMap').setView([latitude, longitude], 12);
-    map.scrollWheelZoom.disable();
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    L.marker([latitude, longitude], { icon: bsIcon }).addTo(map)
-        .bindPopup('<h4>'+ title + '</h4>')
-        .openPopup();
-
     /*News Carousel */
     $(".carousel-news").owlCarousel({
         loop: true,
@@ -55,5 +23,36 @@ $(document).ready(function () {
             }
         }
     });
+
+    /* Spot Map */
+    const themeURL = WPURLS.themeURL;
+    const mapDiv = $('#spotMap');
+    const latitude = mapDiv.data('latitude');
+    const longitude = mapDiv.data('longitude');
+    const title = mapDiv.data('title');
+    const bsIcon = L.icon({
+        iconUrl: themeURL + '/dist/images/marker.png',
+        shadowUrl: themeURL + '/dist/images/marker-shadow.png',
+
+        iconSize: [38, 95], // size of the icon
+        shadowSize: [50, 60], // size of the shadow
+        iconAnchor: [22, 90], // point of the icon which will correspond to marker's location
+        shadowAnchor: [4, 62], // the same for the shadow
+        popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    });
+
+    /* Spot Map */
+    var map = L.map('spotMap').setView([latitude, longitude], 12);
+    map.scrollWheelZoom.disable();
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude], { icon: bsIcon }).addTo(map)
+        .bindPopup('<h4>'+ title + '</h4>')
+        .openPopup();
+
+
 
 });
